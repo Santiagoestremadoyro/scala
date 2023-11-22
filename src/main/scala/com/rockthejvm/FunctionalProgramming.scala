4f8307e3b4a5f6ad94e80914f19eb5d13b040e4c
Santiagoestremadoyro <estremadoyros@gmail.com>
2023-11-22 16:59:07 +0100

    Functionsl

--- src/main/scala/com/rockthejvm/FunctionalProgramming.scala
+++ src/main/scala/com/rockthejvm/FunctionalProgramming.scala
@@ -50,59 +50,44 @@ object FunctionalProgramming extends App {
     }
    */
 
-  // higher-order functions: take functions as args/return functions as results
-  val aMappedList: List[Int] = List(1,2,3).map(x => x + 1) // HOF
-  val aFlatMappedList = List(1,2,3).flatMap { x =>
-    List(x, 2 * x)
-  } // alternative syntax, same as .flatMap(x => List(x, 2 * x))
-  val aFilteredList = List(1,2,3,4,5).filter(_ <= 3) // equivalent to x => x <= 3
+  // higher-order function: take functions as arg/return functions as results.
+  val aMappedList: List[Int] = List(1,2,3).map(x => x+1) //HOF
+  val aFlatMappedList: List(1,2,3).flatMap { X =>
+    List(X, X  2)
+  } //alternative syntax, same as .map(X => List(x, 2* X))
+  val afilterList: List(1,2,3,4,5).filter (_ <= 3) //equivalent to x => x <= 3
 
-  // all pairs between the numbers 1, 2, 3 and the letters 'a', 'b', 'c'
-  val allPairs = List(1,2,3).flatMap(number => List('a', 'b', 'c').map(letter => s"$number-$letter"))
+  // all pairs between the num 1, 2, 3 and the letters "a", "b", "c"
+  val allPairs: List(1,2,3).flatMap(number => List 'a', 'b', 'c').map(letters => s"$number-$letters")
 
-  // for comprehensions
-  val alternativePairs = for {
+  //for comprehension
+  val altNativePairs: for {
     number <- List(1,2,3)
-    letter <- List('a', 'b', 'c')
+    letter <- List('a','b','c')
   } yield s"$number-$letter"
-  // equivalent to the map/flatMap chain above
+  //equivalent to the map/flatmap chain above
 
   /**
-    * Collections
-    */
+   * collections
+   */
 
-  // lists
+  //list
   val aList = List(1,2,3,4,5)
-  val firstElement = aList.head
-  val rest = aList.tail
-  val aPrependedList = 0 :: aList // List(0,1,2,3,4,5)
-  val anExtendedList = 0 +: aList :+ 6 // List(0,1,2,3,4,5,6)
+  val fistElement = alist.head
+  val rest = alist.tail
+  val PrependingList = 0 :: aList // List (0,1,2,3,4,5)
+  val anExtendingLsit = 0 +: aList + 6 // List(0,1,2,3,4,5,6)
 
-  // sequences
-  val aSequence: Seq[Int] = Seq(1,2,3) // Seq.apply(1,2,3)
-  val accessedElement = aSequence(1) // the element at index 1: 2
+  //Sequences
+  val aSequences: seq[Int] = Seq(1,2,3) //Seq.apply(1,2,3
+  val accesElementes = aSequences(1) // the element in the 1 index: 2
 
-  // vectors: fast Seq implementation
-  val aVector = Vector(1,2,3,4,5)
+  //vectors
+  val vector = Vector(1,2,3,4,5)
 
-  // sets = no duplicates
-  val aSet = Set(1,2,3,4,1,2,3) // Set(1,2,3,4)
-  val setHas5 = aSet.contains(5) // false
-  val anAddedSet = aSet + 5 // Set(1,2,3,4,5)
-  val aRemovedSet = aSet - 3 // Set(1,2,4)
-
-  // ranges
-  val aRange = 1 to 1000
-  val twoByTwo = aRange.map(x => 2 * x).toList // List(2,4,6,8..., 2000)
-
-  // tuples = groups of values under the same value
-  val aTuple = ("Bon Jovi", "Rock", 1982)
-
-  // maps
-  val aPhonebook: Map[String, Int] = Map(
-    ("Daniel", 6437812),
-    "Jane" -> 327285 // ("Jane", 327285)
-  )
-
-
+  //set
+  val aSet = Set(1,2,3,1,2,3) //set(1,2,3)
+  val setHas5 = aset.contains(5)
+  val anAddedSet = aSet + 5 // set(1,2,3,5)
+  val aRemoveSet = aSet - 2 //set(1,3)
 }
