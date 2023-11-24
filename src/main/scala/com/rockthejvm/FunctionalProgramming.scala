12ad0fdf70d7a2198753fcbc169ede7f2d5e1b25
santiago <119969620+Santiagoestremadoyro@users.noreply.github.com>
2023-11-24 19:45:37 +0100

    Advance (#10)
    * [T]
    
    * Functionsl
    
    * Patter matching
    
    * advance

--- src/main/scala/com/rockthejvm/FunctionalProgramming.scala
+++ src/main/scala/com/rockthejvm/FunctionalProgramming.scala
@@ -4,51 +4,10 @@ object FunctionalProgramming extends App {
 
   // Scala is OO
 
+  class Person(name: String) {
+
+
   class Person(name : String) {
-
-    def apply(age: Int): Unit = println(s"I have aged $age years")
-  }
-
-  val bob = new Person("bob")
-  bob.apply(43)
-  bob(49) // Invoking bob as a function === bob.apply(49)
-
-  /*
-  Scala runs in JVM
-  Functional Programming:
-  - compose functions
-  - pass functions as args
-  - return functions as results
-
-  conclusion : FunctionX = Function1, Function2, Functions3....Function22
-   */
-
-  val SimpleIncrementor = new Function1[Int, Int] {
-    override def apply(arg: Int): Int = arg + 1
-  }
-
-  SimpleIncrementor(23)
-  SimpleIncrementor.apply(23)
-
-  // all scala FUNCTIONS are instances of the FunctionX TYPES
-
-  //Functions with two arguments and a string return
-  val StringConcatenator = new Function2[String, String, String] {
-    override def apply(arg1: String, arg2: String): String = arg1 + arg2
-  }
-
-  StringConcatenator("I love", "Scala")
-
-  //syntax sugar
-  val doubler: Int => Int = (X: Int) => X * 2
-  doubler(4)// 8
-
-  /*
-  equivalent to:
-  val doubler : Function1[Int, Int] = New Function1[Int, Int] {
-     override del apply(x= Int) = x * 2
-     }
-   */
 
 
   // higher-order function: take functions as arg/return functions as results.
@@ -58,6 +17,12 @@ object FunctionalProgramming extends App {
   } //alternative syntax, same as .map(X => List(x, 2* X))
   val afilterList: List(1,2,3,4,5).filter (_ <= 3) //equivalent to x => x <= 3
 
+  // higher-order function: take functions as arg/return functions as results.
+  val aMappedList: List[Int] = List(1,2,3).map(x => x+1) //HOF
+  val aFlatMappedList: List(1,2,3).flatMap { X =>
+    List(X, X  2)
+  } //alternative syntax, same as .map(X => List(x, 2* X))
+  val afilterList: List(1,2,3,4,5).filter (_ <= 3) //equivalent to x => x <= 3
   // all pairs between the num 1, 2, 3 and the letters "a", "b", "c"
   val allPairs: List(1,2,3).flatMap(number => List 'a', 'b', 'c').map(letters => s"$number-$letters")
 
@@ -104,4 +69,5 @@ object FunctionalProgramming extends App {
     ("daniel", 685234),
     "jane", -> 357951 //equivalent to ("jane", 357951)
   )
-}
+
+
