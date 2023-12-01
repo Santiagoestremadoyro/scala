e7b11855a05f5cd3b2dfd93c14e8bd4f9a7ec138
Santiagoestremadoyro <estremadoyros@gmail.com>
2023-12-01 15:23:51 +0100

    Context

--- src/main/scala/com/rockthejvm/ContextualAbstractionss.scala
+++ src/main/scala/com/rockthejvm/ContextualAbstractionss.scala
@@ -6,8 +6,25 @@ object ContextualAbstractionss {
        */
   val aList = List(2,1,3,4,5)
   val anOrderedList = aList. sorted // (ordering)
+
   //Ordering
-  val decendingOrdering: Ordering[Int] = Ordering.fromLessThan(_ > _) //(a,b) = a > b
+  given descendingOrdering: Ordering[Int] = Ordering.fromLessThan(_ > _) //(a,b) = a > b
+
+  //analogous to an implicit val
+
+  trait Combinator[A] {
+    def combine(x: A, y:A): A
+  }
+
+  def combineAll[A](list: List[A])(using combinator: Combinator[A]): A =
+    list.reduce((a,b) => combinator.combine(a,b))
+
+  given intCombinator: Combinator[Int] = new Combinator[Int] {
+    override def combine(x: Int, y: Int): Int = x + y 
+  }
+  val theSum = combineAll(aList)
+
+
 
 
   def main(array: Array[String]): Unit = {
